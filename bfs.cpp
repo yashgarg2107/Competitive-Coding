fd41bbb1473d9e141a5a857b6bacae110b93e5db
Yash Garg <yash.garg@cohesity.com>
2019-10-12 20:21:00 +0530

    BFS and DFS implementations

---
+++ bfs.cpp
@@ -0,0 +1,26 @@
+// BFS for a graph with adjacency list representation
+
+// Can also use level in case of tree-like graphs
+vector<ll> level(nmax,0);
+
+void bfs(ll idx, vector<vector<ll>> &adj, vector<bool> &visited) {
+
+    queue<ll> q;
+    q.push(idx);
+    visited[idx] = 1;
+
+    while(!q.empty()) {
+        ll idx = q.front();
+        q.pop();
+
+        cout<<idx<<" "<<level[idx]<<endl;
+
+        for(auto i:adj[idx]) {
+            if(!visited[i]) {
+                q.push(i);
+                visited[i] = 1;
+                level[i] = level[idx]+1;
+            }
+        }
+    }
+}
---
+++ dfs.cpp
@@ -0,0 +1,32 @@
+// DFS for a graph with adjacency list representation
+
+void dfs(ll idx, vector<vector<ll>> &adj, vector<bool> &visited) {
+
+    visited[idx] = 1;
+    printf("%lld", idx);
+
+    for(auto i:adj[idx]) {
+        if(!visited[i]) {
+            dfs(i,adj,visited);
+        }
+    }
+}
+
+// DFS for a tree like graph
+
+void dfs_tree(ll idx, ll par, vector<vector<ll>> &adj) {
+    
+    printf("%lld", idx);
+    
+    if(adj[idx].size()==1 && par!=-1) {
+        // leaf node
+        return;
+    }
+
+    for(auto i:adj[idx]) {
+        if(i!=par) {
+            dfs_tree(i,idx,adj);
+        }
+    }
+}
+
