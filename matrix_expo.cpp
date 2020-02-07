621066bfa11b18ecc3206516ec4cecd8faa84fca
Yash Garg <yash.garg@cohesity.com>
2020-02-07 07:51:51 +0530

    Array Exponentiation

--- matrix_expo.cpp
+++ matrix_expo.cpp
@@ -40,3 +40,45 @@ public:
         return res;
     }
 };
+
+// Array Exponentiation.
+
+ll sz;
+
+class Arr {
+public:
+    vector<vector<ll>> A;
+
+    Arr() {
+        A.resize(sz, vector<ll>(3));
+    }
+
+    Arr operator*(const Arr &arr) {
+        Arr res;
+        for(ll i=0;i<sz;i++) {
+            res.A[i][0] = A[arr.A[i][0]][0];
+            res.A[i][1] = min(A[arr.A[i][0]][1], arr.A[i][1]);
+            res.A[i][2] = A[arr.A[i][0]][2] + arr.A[i][2];
+        }
+
+        return res;
+    }
+
+    // Usage Arr::power(arr,n);
+    static Arr power(Arr &arr, ll n) {
+
+    	// Here res initialised with given matrix. So raise to power k-1.
+    	// Or adjust suitably as per requirements.
+        Arr res; res.A = arr.A;
+
+        while(n) {
+            if(n%2) {
+                res = res*arr;
+            }
+            arr = arr*arr;
+            n /= 2;
+        }
+
+        return res;
+    }
+};
